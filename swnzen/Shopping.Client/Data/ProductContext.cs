baabb8666b8a752689400c0787d6be0eacb384a8
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-12-18 23:55:36 +0300

    ProductContext added

---
+++ swnzen/Shopping.Client/Data/ProductContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Client.Data
+{
+    public class ProductContext
+    {
+    }
+}
--- swnzen/Shopping.Client/Models/Product.cs
+++ swnzen/Shopping.Client/Models/Product.cs
@@ -10,9 +10,10 @@ namespace Shopping.Client.Models
         public string Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
-        public string Summary { get; set; }
         public string Description { get; set; }
         public string ImageFile { get; set; }
         public decimal Price { get; set; }
+        public DateTime CreatedTime { get; set; }
+
     }
 }
