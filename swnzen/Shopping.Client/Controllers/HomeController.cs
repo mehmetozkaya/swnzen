2f3de394d8fd656cdde207c826606d596471a3af
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-12-21 14:40:09 +0300

    ShoppingAPIClient

--- swnzen/Shopping.Client/Controllers/HomeController.cs
+++ swnzen/Shopping.Client/Controllers/HomeController.cs
@@ -6,17 +6,21 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Shopping.Client.Controllers
 {
     public class HomeController : Controller
-    {
+    {        
+        private readonly HttpClient _httpClient;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IHttpClientFactory httpClientFactory, ILogger<HomeController> logger)
         {
-            _logger = logger;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+            _httpClient = httpClientFactory.CreateClient("ShoppingAPIClient");
         }
 
         public IActionResult Index()
