82c426f4786bee88dd468bfb0dad8d22f680af8b
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-12-21 12:50:22 +0300

    context added

---
+++ swnzen/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shopping.API.Data;
+using Shopping.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            var rng = new Random();
+            return ProductContext.Products;
+        }
+    }
+}
