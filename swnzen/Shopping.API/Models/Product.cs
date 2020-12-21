734a2499fc81746bfcaffda73bf16dfeb011617d
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-12-21 16:07:07 +0300

    DatabaseSettings

--- swnzen/Shopping.API/Models/Product.cs
+++ swnzen/Shopping.API/Models/Product.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Shopping.API.Models
 {
     public class Product
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
